6228a2a4535ce564e89978091d2882b06ecf321c
SakibAlIslam <sakib35-1680@diu.edu.bd>
2023-04-30 17:51:25 +0600

    layout-design done

--- components/layout/Sidebar/SidebarTweetButton.tsx
+++ components/layout/Sidebar/SidebarTweetButton.tsx
@@ -8,6 +8,9 @@ const SidebarTweetButton = () => {
         <div className='mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer'>
             <FaFeather size={24} color="white"/>
         </div>
+        <div className='mt-6 hidden lg:block px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-90 cursor-pointer transition'>
+            <span className='text-white hidden lg:block text-center text-[20px]'>Tweet</span>
+        </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/Header/Header";
+
 // Note: This is the main page of the application
 export default function Home() {
   return (
-    <div className="text-3xl text-sky-500">
-      Hello world!
-    </div>
+    <>
+      <Header label='Home' />
+    </>
   )
 }
